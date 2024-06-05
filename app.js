1a8a6d2d8f96901c4b4743c7f2a4a8ebcc23516e
rngnnn <rngnnnclk@gmail.com>
2024-06-05 09:29:47 +0200

    button

---
+++ app.js
@@ -0,0 +1,21 @@
+let buton1 = document.getElementById("myButton")
+let sonuc = document.getElementById("result")
+
+// console.log(buton1)
+// console.log(sonuc)
+
+
+//! Callback yazım çeşitleri
+
+buton1.addEventListener("click",function konsol(){
+    console.log("buton1 clicked")
+})
+
+
+//?2. yol(fonksiyon ismi opsiyonel)
+
+buton1.addEventListener("click",function konsol(){
+    console.log("buton1 clicked")
+})
+
+
